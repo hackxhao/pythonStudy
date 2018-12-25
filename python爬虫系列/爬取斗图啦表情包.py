2dc1710bc4c9e574353a5429aa85971be63b7bda
啊昊Boss <arryhao@126.com>
2018-12-25 10:16:50 +0800

    优化效率，加入线程

--- python爬虫系列/爬取斗图啦表情包.py
+++ python爬虫系列/爬取斗图啦表情包.py
@@ -6,9 +6,11 @@ from bs4 import BeautifulSoup
 import bs4
 import requests
 import time
+from multiprocessing.dummy import Pool as ThreadPool
 
 
-def getImg():
+
+def getImg(url):
 
     header = {
         'Accept': "*/*",
@@ -16,22 +18,23 @@ def getImg():
         'Connection': "keep-alive",
         'User-Agent': "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36"
     }
-
-    urls = [
-        "http://www.doutula.com/photo/list/?page={}".format(str(i)) for i in range(1, 3)]
     
     downPath = r"F:\pythonReptile\images"
 
-    for url in urls:
-        response = requests.get(url, headers=header, timeout=30)
-        #print(response.headers)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        datas = soup.find_all(
-            name="img", attrs={"class": "img-responsive lazy image_dta"})
+    response = requests.get(url, headers=header, timeout=30)
+    #print(response.headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    datas = soup.find_all(
+        name="img", attrs={"class": "img-responsive lazy image_dta"})
     for data in datas:
         print("downloading:", data.attrs['data-original'])
         request.urlretrieve(
             data.attrs['data-original'], downPath+'\%s.jpg' % time.time())
 
 if __name__ == '__main__':
-    getImg()
+    pool = ThreadPool(8)
+    urls = [
+        "http://www.doutula.com/photo/list/?page={}".format(str(i)) for i in range(1, 3)]
+    pool.map(getImg, urls)
+    pool.close()
+    pool.join()
